ce5ffa43f56899e08fd1d8ba53cee60f25b8b6d3
harvey2963 <malletrv2@gmail.com>
2019-11-28 18:17:29 +0100

    update presentation

--- src/Form/TripCancelType.php
+++ src/Form/TripCancelType.php
@@ -16,12 +16,18 @@ class TripCancelType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class, ['attr' => ['class' => 'class-css'],'label' => 'Nom de la sortie'])
+            ->add('name', TextType::class, ['attr' => ['class' => 'class-css'],'label' => 'Nom de la sortie :'])
             ->add('date_start', DateTimeType::class, [
-                'label' => 'Date et heure de la sortie'])
-            ->add('organizer')
-            ->add('place')
-            ->add('cancelReason')
+                'label' => 'Date et heure de la sortie :'])
+            ->add('organizer', TextType::class, [
+                'label' => 'Organisateur :'
+            ])
+            ->add('place', TextType::class, [
+                'label' => 'Lieu :'
+            ])
+            ->add('cancelReason', TextType::class, [
+                'label' => 'Motif de l\'annulation :'
+            ])
         ;
     }
 
